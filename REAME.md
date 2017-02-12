c6f7288db11b9614f774b05f4d6521eb9389130b
juliasilge <julia.silge@gmail.com>
2017-02-12 09:18:03 -0700

    Initial commit

---
+++ REAME.md
@@ -0,0 +1,3 @@
+# An Introduction to Shiny
+
+Some slides and apps for my introductory Shiny workshop at the February 2017 [satRday conference in Cape Town, South Africa](http://satrdays.org/capetown2017/)
---
+++ apps/session_one_app/app.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(southafricastats)
+
+mortality_zaf <- mortality_zaf %>%
+    filter(!(indicator %in% c("All causes")))
+
+# body of the UI
+ui <- fluidPage(
+   
+   # application title
+    titlePanel("Mortality in South African Provinces"),
+    
+    # sidebar with input
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "province",
+                        label = "Province:",
+                        choices = unique(mortality_zaf$province),
+                        selected = "Gauteng", 
+                        multiple = TRUE),
+            checkboxInput(inputId = "show_data",
+                          label = "Show table?",
+                          value = FALSE)
+        ),
+        
+        # show the output
+        mainPanel(
+            plotOutput(outputId = "mortalityPlot"),
+            dataTableOutput(outputId = "mortalityTable")
+        )
+    )
+)
+
+# calculations behind the scenes
+server <- function(input, output) {
+
+    output$mortalityPlot <- renderPlot({
+        mortality_zaf %>%
+            filter(province %in% input$province) %>%
+            ggplot(aes(year, deaths, color = indicator)) +
+            geom_line(alpha = 0.8, size = 1.5) +
+            facet_wrap(~province, scales = "free") +
+            theme_minimal(base_size = 18)
+    })
+    
+    output$mortalityTable <- renderDataTable({
+        if(input$show_data){
+            DT::datatable(data = selected_df())
+        }
+    })
+    
+}
+
+# run the application 
+shinyApp(ui = ui, server = server)
+
